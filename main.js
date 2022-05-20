dc5af1ff8dd339029c2c695b8495f237214e8dcd
toomertiger <robert.berezov@gmail.com>
2022-05-19 20:27:21 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// // need to
+// // -have fucntiont aht generates random item out of a HTMLOptGroupElement. think group could be Array
+// // thre  arrays, each full of parts
+// -funcotin that picks random parts in each
+// -
+// -
+// -
+
+
+const dataset1 = ["Boston College","Clemson","Duke","Florida State","Georgia Tech","Louisville","Miami (FL)","North Carolina","NC State","Pittsburgh","Syracuse","Virginia","Virginia Tech","Wake Forest","Notre Dame*","Illinois","Indiana","Iowa","Maryland","Michigan","Michigan State","Minnesota","Nebraska","Northwestern","Ohio State","Penn State","Purdue","Rutgers","Wisconsin","Baylor","Iowa State","Kansas","Kansas State","Oklahoma State","TCU","Texas Tech","West Virginia","Texas**","Oklahoma**","Arizona","Arizona State","California","UCLA","Colorado","Oregon","Oregon State","USC","Stanford","Utah","Washington","Washington State","Alabama","Arkansas","Auburn","Florida","Georgia","Kentucky","LSU","Ole Miss","Mississippi State","Missouri","South Carolina","Tennessee","Texas A&M","Vanderbilt",""]
---
+++ readme.txt
@@ -0,0 +1,23 @@
+ideas
+
+
+-footbal thing
+-
+
+
+
+
+dataset 1
+-teams
+
+
+
+dataset 2
+-verb
+
+
+
+
+dataset 3
+
+noun
